244aa1e1fa8186be838ba7fffd49e6f891198585
lisa <lisa.estebe@gmail.com>
2021-12-10 14:22:11 +0100

    props & couleurs

--- src/App.js
+++ src/App.js
@@ -3,14 +3,23 @@ import './App.css';
 import BpmInstant from './Component/BpmInstant';
 import { useEffect, useState } from 'react';
 
+function randomBpm() {
+  return Math.floor(Math.random()*100) + 70;
+}
+
 function App() {
-  const [measurement, setMeasurement] = useState(0);
+  const [measurements, setMeasurement] = useState([50]);
 
   useEffect(() => {
     setInterval(() => {
-      setMeasurement(BpmInstant);
+      setMeasurement((prev)=>{
+        return [...prev, randomBpm()]
+      });
     }, 2000);
   }, []); 
+
+  const measurement = measurements[measurements.length-1];
+  console.log(measurements)
 
   return (
     <div className="App">
@@ -20,7 +29,8 @@ function App() {
           Bienvenue sur votre moniteur de fréquence cardiaque
         </p>
         <img src={logo} className="App-logo" alt="logo" />
-        <BpmInstant />
+        <BpmInstant 
+        data={measurement} />
       </header>
     </div>
   );
