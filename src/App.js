b4eb4e86704116c867f01b213515169ba21de21a
lisa <lisa.estebe@gmail.com>
2021-12-10 12:03:03 +0100

    Rechargement bpm auto

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import BpmInstant from './Component/BpmInstant';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [measurement, setMeasurement] = useState(0);
+
+  useEffect(() => {
+    setInterval(() => {
+      setMeasurement(BpmInstant);
+    }, 2000);
+  }, []); 
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Heartrate Monitoring</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Bienvenue sur votre moniteur de fréquence cardiaque
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className="App-logo" alt="logo" />
+        <BpmInstant />
       </header>
     </div>
   );
